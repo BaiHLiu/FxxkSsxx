933c8bdcd44addbd55e806f6aa0dd5ce72d84d56
catop <601179193@qq.com>
2021-02-24 17:18:47 +0800

    修改蒙题为A选项 避免无C选项时报错

--- fuck.py
+++ fuck.py
@@ -236,8 +236,8 @@ def BuildAnswerObject(question):
             if i[1] in answer_dictionary[question["title"]]:
                 answer_object["answer"].append(i[0])
     else:
-        print("答案库中不存在该题答案，蒙一个C选项吧")
-        answer_object["answer"] = [question["answer_list"][2][0]]
+        print("答案库中不存在该题答案，蒙一个A选项吧")
+        answer_object["answer"] = [question["answer_list"][0][0]]
 
     return answer_object, question
 
